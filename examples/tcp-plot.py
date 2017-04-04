ec2e2be64e15820648f77c29931095d7ac9ce729
Daniel Zappala <daniel.zappala@gmail.com>
2017-04-04 16:27:05 -0600

    Added graphing functionality.

--- examples/tcp-plot.py
+++ examples/tcp-plot.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import optparse
 import os
@@ -18,7 +19,98 @@ class Plotter:
         ax = df.plot(x="Time",y="Congestion Window")
         # set the axes
         ax.set_xlabel('Time')
-        ax.set_ylabel('Congestion Window')
+        ax.set_ylabel('Congestion Window (bytes)')
+        plt.suptitle("")
+        plt.title("")
+        plt.savefig(filename)
+
+    def queue(self,filename):
+        plt.figure()
+        df = pd.read_csv('queue.csv')
+        size = df[df.Event == 'size'].copy()
+        ax1 = size.plot(x='Time',y='Queue Size')
+        # drop
+        try:
+            drop = df[df.Event == 'drop'].copy()
+            drop['Queue Size']  =  df['Queue Size'] + 1
+            ax = drop.plot(x='Time',y='Queue Size',kind='scatter',marker='x',s=10,ax=ax1)
+        except:
+            pass
+        # set the axes
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Queue Size (packets)')
+        plt.suptitle("")
+        plt.title("")
+        plt.savefig(filename)
+
+    def clear_buckets(self):
+        self.buckets = {}
+
+    def add_to_bucket(self,time,size):
+        bucket = math.trunc(time*10.0)/10.0
+        if bucket not in self.buckets:
+            self.buckets[bucket] = 0
+        self.buckets[bucket] += size*8/(1000000*0.1)
+
+    def rate(self,filename):
+        plt.figure()
+        df = pd.read_csv('rate.csv')
+
+        # flow 1
+        self.clear_buckets()
+        flow = df[df.Flow == 1].copy()
+        for row in flow.itertuples():
+            self.add_to_bucket(row[1],row[3])
+        series = pd.Series(self.buckets)
+        ax = series.plot()
+
+        # flow 2
+        try:
+            self.clear_buckets()
+            flow = df[df.Flow == 2].copy()
+            for row in flow.itertuples():
+                self.add_to_bucket(row[1],row[3])
+            series = pd.Series(self.buckets)
+            series.plot(ax=ax)
+        except:
+            pass
+
+        # flow 3
+        try:
+            self.clear_buckets()
+            flow = df[df.Flow == 3].copy()
+            for row in flow.itertuples():
+                self.add_to_bucket(row[1],row[3])
+            series = pd.Series(self.buckets)
+            series.plot(ax=ax)
+        except:
+            pass
+
+        # flow 4
+        try:
+            self.clear_buckets()
+            flow = df[df.Flow == 4].copy()
+            for row in flow.itertuples():
+                self.add_to_bucket(row[1],row[3])
+            series = pd.Series(self.buckets)
+            series.plot(ax=ax)
+        except:
+            pass
+
+        # flow 5
+        try:
+            self.clear_buckets()
+            flow = df[df.Flow == 5].copy()
+            for row in flow.itertuples():
+                self.add_to_bucket(row[1],row[3])
+            series = pd.Series(self.buckets)
+            series.plot(ax=ax)
+        except:
+            pass
+
+        # set the axes
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Rate (Mbps)')
         plt.suptitle("")
         plt.title("")
         plt.savefig(filename)
@@ -42,7 +134,7 @@ class Plotter:
         # ack
         ack = df[df.Event == 'ack'].copy()
         ax = ack.plot(x='Time',y='Sequence Number',kind='scatter',marker='.',s=2,figsize=(11,3),ax=ax1)
-        ax.set_xlim(-0.1,5)
+        ax.set_xlim(left=-0.01)
         ax.set_xlabel('Time')
         ax.set_ylabel('Sequence Number')
         plt.suptitle("")
@@ -54,5 +146,14 @@ if __name__ == '__main__':
     if not os.path.exists(directory):
         os.makedirs(directory)
     p = Plotter()
-    p.cwnd('graphs/cwnd.png')
-    p.sequence('graphs/sequence.png')
+    p.rate('graphs/rate.png')
+    p.queue('graphs/queue.png')
+    # these graphs may not work with multiple flows
+    try:
+        p.cwnd('graphs/cwnd.png')
+    except:
+        pass
+    try:
+        p.sequence('graphs/sequence.png')
+    except:
+        pass
